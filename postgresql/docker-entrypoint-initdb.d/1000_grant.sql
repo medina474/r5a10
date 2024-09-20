ce8192abb94f82017f2bce911673a94a7c9ee384
emedina <e.medina@neotech.fr>
2024-09-20 15:21:44 +0200

    grant

--- postgresql/docker-entrypoint-initdb.d/1000_grant.sql
+++ postgresql/docker-entrypoint-initdb.d/1000_grant.sql
@@ -1,7 +1,13 @@
 \c northwind
 
+create role directus noinherit login password 'motdepasse';
+grant all privileges on schema public to directus;
+
 grant usage on schema public to clients_web;
 grant select on pays to clients_web;
+grant select on devises to clients_web;
+grant select on langues to clients_web;
+grant select on regions to clients_web;
 
 create role postgrest noinherit login password 'motdepasse';
 grant clients_web to postgrest;

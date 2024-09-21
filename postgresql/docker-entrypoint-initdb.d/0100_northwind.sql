2b584ae3205c89a8e83c0bf12a27738aa1502265
Emmanuel Medina <emmanuel.medina@univ-lorraine.fr>
2024-09-21 06:00:27 +0200

    northwind

--- postgresql/docker-entrypoint-initdb.d/0100_northwind.sql
+++ postgresql/docker-entrypoint-initdb.d/0100_northwind.sql
@@ -7,6 +7,18 @@ drop table if exists pays_langues;
 drop table if exists devises;
 drop table if exists pays_devises;
 
+drop table if exists order_details;
+drop table if exists orders;
+drop table if exists customers;
+drop table if exists products;
+drop table if exists shippers;
+drop table if exists suppliers;
+drop table if exists territories;
+drop table if exists us_states;
+drop table if exists categories;
+drop table if exists region;
+drop table if exists employees;
+
 -- pays
 
 create table if not exists pays
@@ -75,4 +87,116 @@ create table if not exists pays_devises
   devise_code character(3) not null,
   valide_from date default null,
   valide_to date default null
+);
+
+CREATE TABLE categories (
+  category_id smallint NOT NULL,
+  category_name character varying(15) NOT NULL,
+  description text,
+);
+
+CREATE TABLE customers (
+  customer_id character varying(5) NOT NULL,
+  company_name character varying(40) NOT NULL,
+  contact_name character varying(30),
+  contact_title character varying(30),
+  address character varying(60),
+  city character varying(15),
+  region character varying(15),
+  postal_code character varying(10),
+  country character varying(15),
+  phone character varying(24),
+  fax character varying(24)
+);
+
+CREATE TABLE employees (
+    employee_id smallint NOT NULL,
+    last_name character varying(20) NOT NULL,
+    first_name character varying(10) NOT NULL,
+    title character varying(30),
+    title_of_courtesy character varying(25),
+    birth_date date,
+    hire_date date,
+    address character varying(60),
+    city character varying(15),
+    region character varying(15),
+    postal_code character varying(10),
+    country character varying(15),
+    home_phone character varying(24),
+    extension character varying(4),
+    photo bytea,
+    notes text,
+    reports_to smallint,
+    photo_path character varying(255)
+);
+
+CREATE TABLE employee_territories (
+  employee_id smallint NOT NULL,
+  territory_id character varying(20) NOT NULL
+);
+
+CREATE TABLE order_details (
+  order_id smallint NOT NULL,
+  product_id smallint NOT NULL,
+  unit_price real NOT NULL,
+  quantity smallint NOT NULL,
+  discount real NOT NULL
+);
+
+
+CREATE TABLE orders (
+  order_id smallint NOT NULL,
+  customer_id character varying(5),
+  employee_id smallint,
+  order_date date,
+  required_date date,
+  shipped_date date,
+  ship_via smallint,
+  freight real,
+  ship_name character varying(40),
+  ship_address character varying(60),
+  ship_city character varying(15),
+  ship_region character varying(15),
+  ship_postal_code character varying(10),
+  ship_country character varying(15)
+);
+
+CREATE TABLE region (
+    region_id smallint NOT NULL,
+    region_description character varying(60) NOT NULL
+);
+
+
+CREATE TABLE shippers (
+    shipper_id smallint NOT NULL,
+    company_name character varying(40) NOT NULL,
+    phone character varying(24)
+);
+
+CREATE TABLE suppliers (
+    supplier_id smallint NOT NULL,
+    company_name character varying(40) NOT NULL,
+    contact_name character varying(30),
+    contact_title character varying(30),
+    address character varying(60),
+    city character varying(15),
+    region character varying(15),
+    postal_code character varying(10),
+    country character varying(15),
+    phone character varying(24),
+    fax character varying(24),
+    homepage text
+);
+
+CREATE TABLE territories (
+    territory_id character varying(20) NOT NULL,
+    territory_description character varying(60) NOT NULL,
+    region_id smallint NOT NULL
+);
+
+CREATE TABLE us_states (
+    state_id smallint NOT NULL,
+    state_name character varying(100),
+    state_abbr character varying(2),
+    state_region character varying(50)
 );
--- postgresql/docker-entrypoint-initdb.d/0102_constraints.sql
+++ postgresql/docker-entrypoint-initdb.d/0102_constraints.sql
@@ -26,3 +26,181 @@ alter table if exists pays_langues
 
 alter table if exists devises
   add primary key (devise_code);
+
+
+
+ALTER TABLE ONLY categories
+    ADD CONSTRAINT pk_categories PRIMARY KEY (category_id);
+
+
+ALTER TABLE ONLY customers
+    ADD CONSTRAINT pk_customers PRIMARY KEY (customer_id);
+
+
+--
+-- Name: pk_employees; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
+--
+
+ALTER TABLE ONLY employees
+    ADD CONSTRAINT pk_employees PRIMARY KEY (employee_id);
+
+
+--
+-- Name: pk_employee_territories; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
+--
+
+ALTER TABLE ONLY employee_territories
+    ADD CONSTRAINT pk_employee_territories PRIMARY KEY (employee_id, territory_id);
+
+
+--
+-- Name: pk_order_details; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
+--
+
+ALTER TABLE ONLY order_details
+    ADD CONSTRAINT pk_order_details PRIMARY KEY (order_id, product_id);
+
+
+--
+-- Name: pk_orders; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
+--
+
+ALTER TABLE ONLY orders
+    ADD CONSTRAINT pk_orders PRIMARY KEY (order_id);
+
+
+--
+-- Name: pk_products; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
+--
+
+ALTER TABLE ONLY products
+    ADD CONSTRAINT pk_products PRIMARY KEY (product_id);
+
+
+--
+-- Name: pk_region; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
+--
+
+ALTER TABLE ONLY region
+    ADD CONSTRAINT pk_region PRIMARY KEY (region_id);
+
+
+--
+-- Name: pk_shippers; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
+--
+
+ALTER TABLE ONLY shippers
+    ADD CONSTRAINT pk_shippers PRIMARY KEY (shipper_id);
+
+
+--
+-- Name: pk_suppliers; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
+--
+
+ALTER TABLE ONLY suppliers
+    ADD CONSTRAINT pk_suppliers PRIMARY KEY (supplier_id);
+
+
+--
+-- Name: pk_territories; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
+--
+
+ALTER TABLE ONLY territories
+    ADD CONSTRAINT pk_territories PRIMARY KEY (territory_id);
+
+
+--
+-- Name: pk_usstates; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
+--
+
+ALTER TABLE ONLY us_states
+    ADD CONSTRAINT pk_usstates PRIMARY KEY (state_id);
+
+
+--
+-- Name: fk_orders_customers; Type: Constraint; Schema: -; Owner: -
+--
+
+ALTER TABLE ONLY orders
+    ADD CONSTRAINT fk_orders_customers FOREIGN KEY (customer_id) REFERENCES customers;
+
+
+--
+-- Name: fk_orders_employees; Type: Constraint; Schema: -; Owner: -
+--
+
+ALTER TABLE ONLY orders
+    ADD CONSTRAINT fk_orders_employees FOREIGN KEY (employee_id) REFERENCES employees;
+
+
+--
+-- Name: fk_orders_shippers; Type: Constraint; Schema: -; Owner: -
+--
+
+ALTER TABLE ONLY orders
+    ADD CONSTRAINT fk_orders_shippers FOREIGN KEY (ship_via) REFERENCES shippers;
+
+
+--
+-- Name: fk_order_details_products; Type: Constraint; Schema: -; Owner: -
+--
+
+ALTER TABLE ONLY order_details
+    ADD CONSTRAINT fk_order_details_products FOREIGN KEY (product_id) REFERENCES products;
+
+
+--
+-- Name: fk_order_details_orders; Type: Constraint; Schema: -; Owner: -
+--
+
+ALTER TABLE ONLY order_details
+    ADD CONSTRAINT fk_order_details_orders FOREIGN KEY (order_id) REFERENCES orders;
+
+
+--
+-- Name: fk_products_categories; Type: Constraint; Schema: -; Owner: -
+--
+
+ALTER TABLE ONLY products
+    ADD CONSTRAINT fk_products_categories FOREIGN KEY (category_id) REFERENCES categories;
+
+
+--
+-- Name: fk_products_suppliers; Type: Constraint; Schema: -; Owner: -
+--
+
+ALTER TABLE ONLY products
+    ADD CONSTRAINT fk_products_suppliers FOREIGN KEY (supplier_id) REFERENCES suppliers;
+
+
+--
+-- Name: fk_territories_region; Type: Constraint; Schema: -; Owner: -
+--
+
+ALTER TABLE ONLY territories
+    ADD CONSTRAINT fk_territories_region FOREIGN KEY (region_id) REFERENCES region;
+
+
+--
+-- Name: fk_employee_territories_territories; Type: Constraint; Schema: -; Owner: -
+--
+
+ALTER TABLE ONLY employee_territories
+    ADD CONSTRAINT fk_employee_territories_territories FOREIGN KEY (territory_id) REFERENCES territories;
+
+
+--
+-- Name: fk_employee_territories_employees; Type: Constraint; Schema: -; Owner: -
+--
+
+ALTER TABLE ONLY employee_territories
+    ADD CONSTRAINT fk_employee_territories_employees FOREIGN KEY (employee_id) REFERENCES employees;
+
+--
+-- Name: fk_employees_employees; Type: Constraint; Schema: -; Owner: -
+--
+
+ALTER TABLE ONLY employees
+    ADD CONSTRAINT fk_employees_employees FOREIGN KEY (reports_to) REFERENCES employees;
+
+    

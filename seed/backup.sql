1832b352b16ffb5abb145ecaed126739dbcd7dad
Emmanuel Medina <emmanuel.medina@univ-lorraine.fr>
2024-10-25 07:54:35 +0200

    backup

--- seed/backup.sql
+++ seed/backup.sql
@@ -4,25 +4,25 @@
 
 -- Cinema
 
---\copy (select f_id, p_id, role, alias, ordre from cinema.equipes order by f_id, ordre) to '../data/cinema/equipes.csv' (format csv, header, encoding 'utf8');
+\copy (select film_id, personne_id, role, alias, ordre from cinema.equipes order by film_id, ordre) to '../data/cinema/equipes.csv' (format csv, header, encoding 'utf8');
 
---\copy (select p_id, nom, prenom, naissance, deces, nationalite, artiste, popularite from cinema.personnes order by p_id) to '../data/cinema/personnes.csv' (format csv, header, encoding 'utf8');
+\copy (select personne_id, nom, prenom, naissance, deces, nationalite, artiste, popularite from cinema.personnes order by personne_id) to '../data/cinema/personnes.csv' (format csv, header, encoding 'utf8');
 
 \copy cinema.franchises to '../data/cinema/franchises.csv' (format csv, header, encoding 'utf8');
-\copy (select f_id, genre_id from cinema.films_genres order by f_id) to '../data/cinema/films_genres.csv' (format csv, header, encoding 'utf8');
+\copy (select film_id, genre_id from cinema.films_genres order by film_id) to '../data/cinema/films_genres.csv' (format csv, header, encoding 'utf8');
 
-\copy (select id2, site_id, identifiant from cinema.links_societes order by id2) to '../data/cinema/links_societes.csv' (format csv, header, encoding 'utf8');
-\copy (select id2, site_id, identifiant from cinema.links_personnes order by id2) to '../data/cinema/links_personnes.csv' (format csv, header, encoding 'utf8');
-\copy (select id2, site_id, identifiant from cinema.links_films order by id2) to '../data/cinema/links_films.csv' (format csv, header, encoding 'utf8');
+\copy (select id, site_id, identifiant from cinema.links_societes order by id) to '../data/cinema/links_societes.csv' (format csv, header, encoding 'utf8');
+\copy (select id, site_id, identifiant from cinema.links_personnes order by id) to '../data/cinema/links_personnes.csv' (format csv, header, encoding 'utf8');
+\copy (select id, site_id, identifiant from cinema.links_films order by id) to '../data/cinema/links_films.csv' (format csv, header, encoding 'utf8');
 
-\copy (select f_id, titre, titre_original, annee, sortie,duree,franchise_id,vote_votants,vote_moyenne, pays from cinema.films order by f_id) to '../data/cinema/films.csv' (format csv, header, encoding 'utf8');
-\copy (select f_id, slogan from cinema.films where slogan <> '' order by f_id) to '../data/cinema/films_slogan.csv' (format csv, header, delimiter ',', encoding 'utf8');
-\copy (select f_id, langue_code, resume from cinema.resumes where resume <> '' order by f_id) to '../data/cinema/resumes.csv' (format csv, header, delimiter ',', encoding 'utf8');
+\copy (select film_id, titre, titre_original, annee, sortie,duree,franchise_id,vote_votants,vote_moyenne, pays from cinema.films order by film_id) to '../data/cinema/films.csv' (format csv, header, encoding 'utf8');
+\copy (select film_id, slogan from cinema.films where slogan <> '' order by film_id) to '../data/cinema/films_slogan.csv' (format csv, header, delimiter ',', encoding 'utf8');
+\copy (select film_id, langue_code, resume from cinema.resumes where resume <> '' order by film_id) to '../data/cinema/resumes.csv' (format csv, header, delimiter ',', encoding 'utf8');
 
-\copy (select s_id, societe, uei from cinema.societes order by s_id) to '../data/cinema/societes.csv' (format csv, header, encoding 'utf8');
-\copy (select f_id, s_id from cinema.productions order by f_id, s_id) to '../data/cinema/productions.csv' (format csv, header, encoding 'utf8');
+\copy (select societe_id, societe, uei from cinema.societes order by societe_id) to '../data/cinema/societes.csv' (format csv, header, encoding 'utf8');
+\copy (select film_id, societe_id from cinema.productions order by film_id, societe_id) to '../data/cinema/productions.csv' (format csv, header, encoding 'utf8');
 
-\copy (select f_id, motcle_id from cinema.films_motscles order by f_id, motcle_id) to '../data/cinema/films_motscles.csv' (format csv, header, encoding 'utf8');
+\copy (select film_id, motcle_id from cinema.films_motscles order by film_id, motcle_id) to '../data/cinema/films_motscles.csv' (format csv, header, encoding 'utf8');
 
 \copy cinema.motscles to '../data/cinema/motscles.csv' (format csv, header, encoding 'utf8');
 
